485960f65b855a9ccb4ec3d5b814fbaca9169797
Jontes-Tech <jonatan@jontes.page>
2022-03-09 12:41:57 +0100

    fixing favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <title>Jonte</title>
-        <link rel="icon" href="favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
         <h1 className={styles.title}>
