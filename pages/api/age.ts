2d22d893d3ac989c2ae6868621c1686d181b3e06
Jontes-Tech <jonatan@jontes.page>
2022-03-18 21:23:56 +0100

    Kinda works

---
+++ pages/api/age.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  res.status(200).json({ age: age})
+}
+var dob = '20090201';
+var year = Number(dob.substr(0, 4));
+var month = Number(dob.substr(4, 2)) - 1;
+var day = Number(dob.substr(6, 2));
+var today = new Date();
+var age = today.getFullYear() - year;
+if (today.getMonth() < month || (today.getMonth() == month && today.getDate() < day)) {
+  age--;
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,17 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Particles from 'react-particles-js';
 
+class App extends Component{
+  
+    render(){
+        return (
+            <Particles />
+        );
+    };
+
+}
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
