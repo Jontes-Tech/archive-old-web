32cc3931b4a7ca4286d1598fa9b460715ca945b9
Jontes-Tech <jonatan@jontes.page>
2022-03-18 21:39:34 +0100

    Hope this works

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
@@ -27,30 +26,8 @@ const Home: NextPage = () => {
             <h2>Check out my Git! &rarr;</h2>
             <p>Plagiarism is fine, right?!</p>
           </a>
-
-          {/* <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a> */}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-          Built with (very little) ❤️ by Jonte - Licensed under GPL 3
-      </footer>
     </div>
   )
 }
