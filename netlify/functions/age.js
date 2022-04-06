9d5d06b1f485712f059b42d4aac9f01a71439339
Jontes-Tech <jonatan@jontes.page>
2022-04-06 14:55:44 +0200

    Removing bad api

--- netlify/functions/age.js
+++
@@ -1,7 +0,0 @@
-var myage =((Date.now() / 1000 - 1233516011) / 31557600);
-exports.handler = async function (event, context) {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({age: myage}),
-    };
-  }
