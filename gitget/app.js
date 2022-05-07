70bf0bc14b72a36ac8bd7ead886de2bedf858b70
jonte <jonatan@jontes.page>
2022-05-07 13:08:38 +0200

    Remove failiure

--- gitget/app.js
+++
@@ -1,9 +0,0 @@
-function pageOnline() {
-    console.log("Working")
-    window.location.replace("./ssh_key");
-  }
-function pageOffline() {
-    console.log("Not Working")
-    setTimeout(function(){
-      location.reload();}, 5000);
-}
--- gitget/config/app.js
+++
@@ -1,3 +0,0 @@
-let username = prompt("Let's Configure it your Username!", "Enter Username");
-let email = prompt("Let's Configure it your Email", "Enter Mail");
-fetch('http://localhost:8080/config?mail=' + email + "&name=" + username)
